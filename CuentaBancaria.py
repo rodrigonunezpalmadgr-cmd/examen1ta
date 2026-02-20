0b78244f93ca41de6021280c419a48d7993eadda
Rodri77 <rodrigo.nunez@exxis-group.com>
2026-02-19 21:43:36 -0400

    fix:ajuste de cuentasbancarias01

--- CuentaBancaria.py
+++ CuentaBancaria.py
@@ -25,19 +25,30 @@ class CuentaBancaria:
         print(f"Saldo actual: {self.saldo}")
 
     def crear_cuenta():
+        titular = input("Ingrese el nombre del titular de la cuenta: ")
+        numero_cuenta = input("Ingrese el número de cuenta: ")  
+        cuenta = CuentaBancaria(titular, 0, numero_cuenta)
+
         print("Cuenta creada correctamente")
 
-    def mostrar_cuenta():
-        print("Mostrar cuenta")        
+    def mostrar_cuenta(self):
+        print(f"Titular: {self.titular}")
+        print(f"Número de cuenta: {self.numero_cuenta}")
+        print(f"Saldo: {self.saldo}")
+
 
 print("=======MENNU==========")
-print("Crear Cuenta")
-print("Mostrar Cuenta")
-print("Depositar Dinero")
-print("Retirar Dinero")
-print("Salir")
+print("1: Crear Cuenta")
+print("2: Mostrar Cuenta")
+print("3: Depositar Dinero")
+print("4: Retirar Dinero")
+print("5: Salir")
 
 opcion = int(input("Elige una opcion del 1 al 5: "))
+while opcion < 1 or opcion > 5:
+    print("Opcion no valida, elige una opcion del 1 al 5")
+    opcion = int(input("Elige una opcion del 1 al 5: "))
+
 if opcion == 1:
     CuentaBancaria.crear_cuenta()
 elif opcion == 2:
@@ -47,5 +58,5 @@ elif opcion == 3:
 
 elif opcion == 4:
     CuentaBancaria.retirar()
-else:
-    print("Esta opcion no es valida")                      
+elif opcion == 5:
+    print("Saliendo del programa...")
